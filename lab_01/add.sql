1d58160d860ad4a5ed443f134f2798705a69bfd6
romanov.alexey2000@gmail.com <romanov.alexey2000@gmail.com>
2020-09-18 13:29:20 +0300

    add: defence"

---
+++ lab_01/add.sql
@@ -0,0 +1,13 @@
+ALTER TABLE errors 
+ADD (
+    percentage FLOAT CHECK(percentage BETWEEN 0 AND 100),
+    danger_level NUMBER(3) CHECK(danger_level BETWEEN 1 AND 10),
+    price INTEGER CHECK(price > 0),
+    created DATE
+);
+
+ALTER TABLE analyzers
+ADD (
+    price INTEGER CHECK(price > 0),
+    downloads INTEGER CHECK(downloads > 0)
+);
